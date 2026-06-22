f7360aba9e64f16ae0032127e5453fc05e16103c
Azarudeen B <118591563+azar011@users.noreply.github.com>
2026-06-22 10:32:12 +0530

    Update speech_service.py

--- Speech_recognition/app/services/speech_service.py
+++ Speech_recognition/app/services/speech_service.py
@@ -6,8 +6,8 @@ load_dotenv()
 MODEL_TYPE = os.getenv("MODEL_TYPE", "whisper")
 
 if MODEL_TYPE == "seamless":
-    from Rlst.Speech_recognition.app.services.seamless_service import translate_audio
+    from app.services.seamless_service import translate_audio
 
 else:
-    from Rlst.Speech_recognition.app.services.whisper_service import translate_audio
+    from app.services.whisper_service import translate_audio
     
